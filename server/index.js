b2c556aec8da0f6be5fffc21ba388d3987144d1e
Alejandro <alexsotog82@gmail.com>
2020-04-04 16:18:13 -0600

    se sube back

---
+++ server/index.js
@@ -0,0 +1,8 @@
+const express = require('express'),
+      bodyParser = require('body-parser'),
+      mongoose = require('mongoose'),
+      app = express(),
+      cors = require('cors');
+
+
+// conectar la base de datos
